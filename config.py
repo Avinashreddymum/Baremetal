06cc1c6f17c8539d8619f4b3aa3ec368bed6454b
Avinashreddymum <avinash.reddy@nttcomcloud.com>
2017-12-27 07:40:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+class DailyDb:
+    host = 'localhost'
+    username = 'root'
+    password = 'redhat'
+    database = 'bm'
+
+class RealTimeDb:
+    host = 'localhost'
+    username = 'root'
+    password = 'redhat'
+    database = 'BM_realtime'
